19e5c984a769238b5ceca077f0acd4ef14b51577
David Vieiro Pazos <davidvieiro@gmail.com>
2024-02-01 13:33:26 +0100

    Cambiada una variable a constante

--- pruebas_javascript.js
+++ pruebas_javascript.js
@@ -81,58 +81,58 @@ function limitRandomIntInterval(limit, min, max) {
     let numGenerados = []; // definimos la variable como array vacio
     let errores = 0; // comprobamos si hay errores en los campos de texto
     // definimos la variable con el elemento HTML ya que lo vamos a usar muchas veces
-    elementoNumGenerados = document.getElementById("numGenerados");
+    const ELEMENTO_NUM_GENERADOS = document.getElementById("numGenerados");
     // limpiamos el contenido del elemento HTML con id numGenerados por si se vuelve a ejecutar la funcion
-    elementoNumGenerados.innerHTML = "";
+    ELEMENTO_NUM_GENERADOS.innerHTML = "";
     // establecemos el color del texto en rojo por si saltan errores
-    elementoNumGenerados.style.color = "red";
+    ELEMENTO_NUM_GENERADOS.style.color = "red";
 
     // comprobamos que el valor del numero limite sea cero o inferior y devolvemos un error si ese es el caso
     if (limit <= 0) {
         errores++;
-        elementoNumGenerados.append("El valor del numero limite no puede ser 0 o inferior. ");
-        elementoNumGenerados.append(document.createElement("br"));
+        ELEMENTO_NUM_GENERADOS.append("El valor del numero limite no puede ser 0 o inferior. ");
+        ELEMENTO_NUM_GENERADOS.append(document.createElement("br"));
     }
     // comprobamos que el valor del numero minimo sea inferior a cero y devolvemos un error si ese es el caso
     if (min < 0) {
         errores++;
-        elementoNumGenerados.append("El valor del numero minimo no puede ser inferior a 0. ");
-        elementoNumGenerados.append(document.createElement("br"));
+        ELEMENTO_NUM_GENERADOS.append("El valor del numero minimo no puede ser inferior a 0. ");
+        ELEMENTO_NUM_GENERADOS.append(document.createElement("br"));
     }
     // comprobamos que el valor del numero maximo sea cero o inferior y devolvemos un error si ese es el caso
     if (max <= 0) {
         errores++;
-        elementoNumGenerados.append("El valor del numero maximo no puede ser 0 o inferior. ");
-        elementoNumGenerados.append(document.createElement("br"));
+        ELEMENTO_NUM_GENERADOS.append("El valor del numero maximo no puede ser 0 o inferior. ");
+        ELEMENTO_NUM_GENERADOS.append(document.createElement("br"));
     }
     // comprobamos que el valor del numero maximo sea mayor que 10000 y devolvemos un error si ese es el caso
     if (max > 10000) {
         errores++;
-        elementoNumGenerados.append("El valor del numero maximo no puede superar 10000. ");
-        elementoNumGenerados.append(document.createElement("br"));
+        ELEMENTO_NUM_GENERADOS.append("El valor del numero maximo no puede superar 10000. ");
+        ELEMENTO_NUM_GENERADOS.append(document.createElement("br"));
     }
     // comprobamos que el valor del numero minimo sea mayor que 10000 y devolvemos un error si ese es el caso
     if (min > 10000) {
         errores++;
-        elementoNumGenerados.append("El valor del numero minimo no puede superar 10000. ");
-        elementoNumGenerados.append(document.createElement("br"));
+        ELEMENTO_NUM_GENERADOS.append("El valor del numero minimo no puede superar 10000. ");
+        ELEMENTO_NUM_GENERADOS.append(document.createElement("br"));
     }
     // comprobamos que el valor del numero limite sea mayor que 10000 y devolvemos un error si ese es el caso
     if (limit > 10000) {
         errores++;
-        elementoNumGenerados.append("El valor del numero limite no puede superar 10000. ");
-        elementoNumGenerados.append(document.createElement("br"));
+        ELEMENTO_NUM_GENERADOS.append("El valor del numero limite no puede superar 10000. ");
+        ELEMENTO_NUM_GENERADOS.append(document.createElement("br"));
     }
     // comprobamos que el valor del numero maximo sea menor que el minimo y devolvemos un error si ese es el caso
     if (max < min) {
         errores++;
-        elementoNumGenerados.append("El valor del numero maximo no puede ser inferior al valor del numero minimo. ");
-        elementoNumGenerados.append(document.createElement("br"));
+        ELEMENTO_NUM_GENERADOS.append("El valor del numero maximo no puede ser inferior al valor del numero minimo. ");
+        ELEMENTO_NUM_GENERADOS.append(document.createElement("br"));
     }
     // si no hay errores ejecutamos el numero aleatorio
     if (errores == 0) {
         // sabemos que no hay errores asi que ponemos el color del texto por defecto
-        elementoNumGenerados.style.color = "";
+        ELEMENTO_NUM_GENERADOS.style.color = "";
         // creamos un bucle para generar una cantidad de numeros aleatorios hasta llegar al limite establecido
         for (let i = 0; i < limit; i++) {
             // cada numero que generamos lo insertamos al final del array
@@ -140,8 +140,8 @@ function limitRandomIntInterval(limit, min, max) {
             numGenerados.push(randomIntFromInterval(Number(min), Number(max)));
         }
         // mostramos el array en el parrafo con id "numGenerados" convertido a String
-        elementoNumGenerados.innerHTML = numGenerados;
-        elementoNumGenerados.append(document.createElement("br"));
+        ELEMENTO_NUM_GENERADOS.innerHTML = numGenerados;
+        ELEMENTO_NUM_GENERADOS.append(document.createElement("br"));
     }
 }
 // funcion para limipiar el output de los numeros aleatorios generados
